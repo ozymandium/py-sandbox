fc84980e8859a618925052dc6f19d5eb09fcb16d
Robert Cofield <robertgcofield@gmail.com>
2013-01-17 21:12:17 -0600

    initial commit of pycallgraph decorator

---
+++ rgc_decorators/pycallgraph_deco.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+Decorator for making python function call graphs
+Does not terminate after single iteration
+currently Python v2.7.3
+"""
+try:
+    import pycallgraph
+except ImportError:
+    print('You do not have pycallgraph installed')
+    sys.exit()
+import pdb
+
+
+def callgraph(outfile='/tmp/pycallgraph.png'):
+    def argwrapper(fn):
+        def callwrapper(*args, **kwargs):
+            if not outfile: # allow deactivating
+                return fn(*args, **kwargs)
+            pycallgraph.start_trace()
+            fn_output = fn(*args, **kwargs)
+            pycallgraph.stop_trace()
+            pycallgraph.make_dot_graph(outfile)
+            return fn_output
+        return callwrapper
+    return argwrapper
+
+
+# Example of usage with an output
+@callgraph(outfile='./test_pycallgraph_decorator.png')
+def example_fn1(x):
+    print('this will create a call graph')
+    print(x)
+
+# if you want to leave the decorator but only use it conditionally
+@callgraph(outfile=None)
+def example_fn2():
+    print('This will not')
+
+if __name__ == '__main__':
+    example_fn1(2)
+    example_fn2()
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(  name='py_sandbox',
+        version='0.1',
+        description='Common Python Functions',
+        author='Robert Cofield',
+        author_email='robertgcofield@gmail.com',
+        # packages=['rgc_decorators'],
+        py_modules=['rgc_decorators.pycallgraph_deco'],
+)
